a687ca7840f7641d9348529951e156b759d419e3
RationalDysaniaer <122082502+RationalDysaniaer@users.noreply.github.com>
2024-12-19 19:00:55 +0800

    safeMap

--- p1-SafeMap/byChan/safeMap.go
+++ p1-SafeMap/byChan/safeMap.go
@@ -1,31 +1,67 @@
-package byChan
+package main
 
-// --- todo -- 不会写
+import "fmt"
 
 type SafeMap struct {
 	myMap map[string]interface{}
-	c     chan struct{}
+	ops   chan func()
 }
 
 func NewSafeMap() *SafeMap {
-	return &SafeMap{
+	sm := &SafeMap{
 		myMap: make(map[string]interface{}),
-		c:     make(chan struct{}),
+		ops:   make(chan func()),
+	}
+	go sm.run()
+	return sm
+}
+
+func (sm *SafeMap) run() {
+	for op := range sm.ops {
+		op()
 	}
 }
 
-//func (m *SafeMap) Get() {
-//
-//}
-//
-//func (m *SafeMap) Set() {
-//
-//}
-//
-//func (m *SafeMap) Del() {
-//
-//}
-//
-//func (m *SafeMap) Len() {
-//
-//}
+func (sm *SafeMap) Set(key string, val interface{}) {
+	sm.ops <- func() {
+		sm.myMap[key] = val
+	}
+}
+
+func (sm *SafeMap) Get(key string) (val interface{}, exists bool) {
+	type res struct {
+		val    interface{}
+		exists bool
+	}
+	resChan := make(chan res)
+	sm.ops <- func() {
+		val, exists = sm.myMap[key]
+		resChan <- res{val, exists}
+	}
+	r := <-resChan
+	return r.val, r.exists
+}
+
+func (sm *SafeMap) Del(key string) {
+	sm.ops <- func() {
+		delete(sm.myMap, key)
+	}
+}
+
+func (sm *SafeMap) Len() int {
+	l := make(chan int)
+	sm.ops <- func() {
+		l <- len(sm.myMap)
+	}
+	return <-l
+}
+
+func main() {
+	m := NewSafeMap()
+	m.Set("name", "JustGopher")
+	v, exists := m.Get("name")
+	fmt.Println(v, exists)
+	fmt.Println(m.Len())
+	m.Del("name")
+	fmt.Println(m.Len())
+}
